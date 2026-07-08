5cec194551e5d88ebeff7c55a567477897e5abf7
thanveer761 <thanveer761@gmail.com>
2026-07-08 19:27:13 +0530

    find first and last occurence in strin

--- src/SlidingWindow/MaxSumSubArrayOfSizeK.java
+++ src/SlidingWindow/MaxSumSubArrayOfSizeK.java
@@ -10,7 +10,20 @@ public class MaxSumSubArrayOfSizeK {
         int k = 3;
         Assertions.assertEquals(16, maxSumSubArray(nums, k));
     }
+/*
+ * initialise a variable maxSum=0, sum=0, left=0 to keep track of running sum and max value
+ * iterate the array of window size (k), compute running sum
+ * compare the sum with maxSum and update as needed
+ * iterate the array from size k till the end
+ * - add the element at the current index
+ * - remove the k-left element from the window, so that the window size stays constant
+ * - keep computing the max sum
+ * return the max sum
+ *
+ *  TC: O(n)
+ *  SC: O(1)
 
+ */
     private int maxSumSubArray(int[] nums, int k) {
         int sum=0,maxSum=0;
         for (int i = 0; i < k; i++) {
