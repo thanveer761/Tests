eaa07551a8ca74fd838652dddf14fdb695e5ee2d
thanveer761 <thanveer761@gmail.com>
2026-07-21 20:33:43 +0530

    letter pair builder

--- src/Arrays/SearchInsertPosition.java
+++ src/Arrays/SearchInsertPosition.java
@@ -19,6 +19,25 @@ public class SearchInsertPosition {
         System.out.println(op1);
     }
 
+    /*
+* GOAL: find the index of target in a sorted array nums,
+*        or the index where it should be inserted to keep the array sorted
+
+* traverse using pointer i from 0 to nums.length-1
+    * if nums[i] == target:
+        - target already exists at this position
+        - return i
+
+    * if nums[i] > target:
+        - we've found the first element bigger than target
+        - target belongs right here (before this element)
+        - return i
+
+* if the loop completes without returning:
+    - target is bigger than every element in the array
+    - it belongs at the very end
+    - return nums.length
+*/
         public int searchInsert(int[] nums, int target) {
             for (int i = 0; i < nums.length; i++) {
                 if (nums[i] == target) {  // target found, return its index
