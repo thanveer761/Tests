c38d151091d454aaa57fb26f182b00db8fc01331
thanveer761 <thanveer761@gmail.com>
2026-07-24 20:39:16 +0530

    longest nice substring

--- src/Strings/IsomorphicStrings.java
+++ src/Strings/IsomorphicStrings.java
@@ -52,17 +52,14 @@ public class IsomorphicStrings {
 
             HashMap<Character,Character> hm = new HashMap<>();
             int n=s.length();
-            for(int i=0;i<n;i++)
-            {
+            for(int i=0;i<n;i++) {
                 char ch1 = s.charAt(i);
                 char ch2 = t.charAt(i);
-                if(hm.containsKey(ch1))
-                {
+                if(hm.containsKey(ch1)) {
                     if(hm.get(ch1)!=ch2)
                         return false;
                 }
-                else if(hm.containsValue(ch2))
-                {
+                else if(hm.containsValue(ch2)) {
                     return false;
                 }
                 hm.put(ch1,ch2);
