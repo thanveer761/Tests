b21281b42e5e34a81a5b675925ae97f57dba1e30
thanveer761 <thanveer761@gmail.com>
2026-07-26 20:06:44 +0530

    longest nice substring

--- src/StringOccurences/OccurenceOfCharacter.java
+++ src/StringOccurences/OccurenceOfCharacter.java
@@ -9,31 +9,31 @@ public class OccurenceOfCharacter {
     @Test
     public void test1() {
         String s = "ThanveerAhmed";
-        String op = lastNonRepeatingCharacterString(s);
+        String op = occurenceOfCharacter(s);
         System.out.println(op);
     }
 
     @Test
     public void test2() {
         String s = "aabbcc";
-        String op = lastNonRepeatingCharacterString(s);
+        String op = occurenceOfCharacter(s);
         System.out.println(op);
     }
 
-    private String lastNonRepeatingCharacterString(String s) {
+    private String occurenceOfCharacter(String s) {
         Map<Character, Integer> mn = new HashMap<>();
         for (int i = 0; i < s.length(); i++) {
             mn.put(s.charAt(i), mn.getOrDefault(s.charAt(i), 0) + 1);
         }
         // Print each character with its frequency
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
             if (mn.containsKey(ch)) {
-                System.out.println(ch + "-" + mn.get(ch));
+                result.append(ch).append("-").append(mn.get(ch)).append(" ");
                 mn.remove(ch); // remove to avoid printing duplicates
             }
         }
-        return result.trim();
+        return result.toString().trim();
     }
 }
