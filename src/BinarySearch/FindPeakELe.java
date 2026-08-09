96d76bd314981a8b1de48a05019223e57106b027
thanveer761 <thanveer761@gmail.com>
2026-08-09 19:36:41 +0530

    stack

--- src/BinarySearch/FindPeakELe.java
+++ src/BinarySearch/FindPeakELe.java
@@ -29,7 +29,27 @@ public class FindPeakELe {
         int[] nums={5,4,3,2,1};
         Assertions.assertEquals(0, findPeak(nums));
     }
+    /*
+    * GOAL: find the index of a peak element (an element strictly greater
+    *        than its neighbors), using binary search
 
+    * initialize low = 0, high = n-1
+
+    * while low < high:
+        * mid = (low + high) / 2
+
+        * if nums[mid] < nums[mid+1]:
+            - we're on an "uphill" slope, a peak must exist somewhere to the right
+            - move low = mid + 1
+
+        * else:
+            - we're on a "downhill" slope (or mid itself is already a peak)
+            - move high = mid
+              (keep mid in range, since it could be the peak)
+
+    * when low == high, that index holds a peak
+    * return low
+    */
     public int findPeak(int[] nums){
         int low = 0, high = nums.length - 1;
         while (low < high) {
